e2580e7fe5fd98a2223f14d0180a46aba7449b8e
kerr1gan <2207683406@qq.com>
2024-12-25 23:08:41 +0800

    finish

--- exercises/31_std_shared_ptr/main.cpp
+++ exercises/31_std_shared_ptr/main.cpp
@@ -10,36 +10,40 @@ int main(int argc, char **argv) {
     std::shared_ptr<int> ptrs[]{shared, shared, shared};
 
     std::weak_ptr<int> observer = shared;
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 4, "");
 
     ptrs[0].reset();
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 3, "");
 
     ptrs[1] = nullptr;
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 2, "");
 
     ptrs[2] = std::make_shared<int>(*shared);
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 1, "");
 
     ptrs[0] = shared;
     ptrs[1] = shared;
     ptrs[2] = std::move(shared);
-    ASSERT(observer.use_count() == ?, "");
+    //! `shared` 已经被移动，不再指向任何对象
+    //! shared = nullptr;
+    //! ptr0, ptr1还会继续指向原来的对象
+    ASSERT(observer.use_count() == 3, "");
 
-    std::ignore = std::move(ptrs[0]);
+    std::ignore = std::move(ptrs[0]);   // 并没有转移所有权， `ptrs[0]` 仍然指向原来的对象
     ptrs[1] = std::move(ptrs[1]);
     ptrs[1] = std::move(ptrs[2]);
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 2, "");
 
     shared = observer.lock();
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 3, "");
 
     shared = nullptr;
     for (auto &ptr : ptrs) ptr = nullptr;
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 0, "");
 
+    // 对象已经被释放，无法再次获取
     shared = observer.lock();
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 0, "");
 
     return 0;
 }

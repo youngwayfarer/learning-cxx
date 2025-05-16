607ada925cc1bb38f7d5ff8835267884d1ce3974
kerr1gan <2207683406@qq.com>
2025-05-16 15:20:45 +0800

    fix exercise 7

--- exercises/07_loop/main.cpp
+++ exercises/07_loop/main.cpp
@@ -8,8 +8,9 @@ static unsigned long long fibonacci(int i) {
     static unsigned long long cache[96], cached;
     cache[0] = 0;
     cache[1] = 1;
+    cached = 2;
     // TODO: 设置正确的循环条件
-    for (cached = 2; cached <= 95; ++cached) {
+    for (; int(cached) <= i; ++cached) {
         cache[cached] = cache[cached - 1] + cache[cached - 2];
     }
     return cache[i];
